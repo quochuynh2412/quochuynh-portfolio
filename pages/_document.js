730f7e3f3c46b7b939be9e65af1de0af43766c9f
Quochuynh <letrinhquoch@gmail.com>
2022-11-25 01:27:34 +0700

    it's not over yet

--- pages/_document.js
+++ pages/_document.js
@@ -2,23 +2,26 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 import packageJson from "../package.json";
 
 class CustomDocument extends Document {
-  render = () => (
-    <Html lang="en">
-      <Head>
-        <meta name="theme-color" content="#000000" />
-        <meta name="build version" content={packageJson.version} />
-        <link
-          rel="preload"
-          href="/font/L'Hanoienne-Demo.ttf"
-          as="font"
-          crossOrigin="anonymous"></link>
-      </Head>
-      <body>
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    render = () => (
+        <Html lang="en">
+            <Head>
+                <meta name="theme-color" content="#000000" />
+                <meta name="build version" content={packageJson.version} />
+                <link
+                    rel="preload"
+                    href="/font/L'Hanoienne-Demo.ttf"
+                    as="font"
+                    crossOrigin="anonymous"></link>
+                <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+                <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin></link>
+                <link href="https://fonts.googleapis.com/css2?family=Lato:ital,wght@0,100;0,300;0,400;0,700;0,900;1,100;1,300;1,400;1,700;1,900&family=Whisper&display=swap" rel="stylesheet"></link>
+            </Head>
+            <body>
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
 
 export default CustomDocument;
